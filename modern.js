9401b2ead7301ff089b05178c8554d70590f0c3e
Malte Legenhausen <mlegenhausen@gmail.com>
2020-04-09 12:30:12 +0200

    Closed #5

--- modern.js
+++ modern.js
@@ -54,6 +54,7 @@ module.exports = {
       }
     ],
     '@typescript-eslint/no-empty-interface': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-parameter-properties': 'off',
     '@typescript-eslint/no-var-requires': 'off'
   }
