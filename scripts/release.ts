6c844d830c82a8367e7fb838e84e276b7b76af05
mlegenhausen <malte@werk85.de>
2021-11-11 10:13:10 +0100

    Version bumped to 5.0.0

--- scripts/release.ts
+++ scripts/release.ts
@@ -1,4 +1,4 @@
 import cp from 'child_process'
 import pkg from '../package.json'
 
-cp.execFileSync('yarn', ['publish', '--new-version', pkg.version, '--tag', 'beta'], { cwd: 'lib', stdio: 'inherit' })
+cp.execFileSync('yarn', ['publish', '--new-version', pkg.version], { cwd: 'lib', stdio: 'inherit' })
