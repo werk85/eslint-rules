9e7f8205d6a7329d2c310787a7d7af6c26cff5cf
mlegenhausen <malte@werk85.de>
2022-03-24 12:35:22 +0100

    react rules disabled that collide with jsx transforms. Closed #33

--- src/react.ts
+++ src/react.ts
@@ -15,7 +15,9 @@ const config: Linter.Config = {
   rules: {
     'react/display-name': 'off',
     'react/jsx-key': 'off',
-    'react/prop-types': 'off'
+    'react/jsx-uses-react': 'off',
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off'
   }
 }
 
